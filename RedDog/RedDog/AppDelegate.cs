02bb582bb17790ac97a64ccaa59c829208bc345a
Mikhail Terentev <k4thix@gmail.com>
2024-06-13 10:06:45 +0500

    Test commit

--- RedDog/RedDog/AppDelegate.cs
+++ RedDog/RedDog/AppDelegate.cs
@@ -12,12 +12,12 @@ namespace RedDog
 
 		public override void DidFinishLaunching (NSNotification notification)
 		{
-			// Insert code here to initialize your application
+
 		}
 
 		public override void WillTerminate (NSNotification notification)
 		{
-			// Insert code here to tear down your application
+
 		}
 	}
 }
